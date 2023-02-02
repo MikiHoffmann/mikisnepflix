1ddcfbea30a7afa90dd4c9c5c842ea6c2c963799
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2023-02-02 13:18:37 +0100

    navbar language

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,20 @@
 import "./NavBar.scss"
 import { Link } from "react-router-dom"
 import { NetflixLogo } from "../Logos/NetflixLogo"
+import { useContext } from "react"
+import { LangContext } from "../../redux/language/languageContext"
 
 const NavBar = () => {
+  const { language } = useContext(LangContext)
   return (
     <>
       <div className="navBarContainer">
         <Link to="/">
           <NetflixLogo />
         </Link>
-        <Link to="/home">Home</Link>
+        <Link to="/home">
+          <span>{language === "EN" ? "Homepage" : "Homepagina"}</span>
+        </Link>
       </div>
     </>
   )
